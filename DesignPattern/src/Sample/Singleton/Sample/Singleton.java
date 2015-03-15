87916c3b386a2b8583f77ae09f971a63245e1a04
mainPC <User@Gaming-PC>
2015-03-15 11:15:27 +0900

    第７章まで完了

--- DesignPattern/src/Sample/Singleton/Sample/Singleton.java
+++ DesignPattern/src/Sample/Singleton/Sample/Singleton.java
@@ -1,6 +1,8 @@
+package Sample.Singleton.Sample;
+
 public class Singleton {
     private static Singleton singleton = new Singleton();
-    private Singleton() {                                 
+    private Singleton() {
         System.out.println("インスタンスを生成しました。");
     }
     public static Singleton getInstance() {
